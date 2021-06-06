788afb2f4b1274c6f964a2bd2d1af90d5e563bf1
Ayush Yajnik <ayush.yajnik@yahoo.com>
2021-06-06 13:03:06 -0400

    db setup and serializing the object in json object

--- app.py
+++ app.py
@@ -1,16 +1,71 @@
 ## importing modules
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, String, Integer, Float
 import os
+from flask_marshmallow import Marshmallow
 
 # initializing the app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] =   "sqllite:///" + os.apth.join(basedir,"planets.db")
+app.config['SQLALCHEMY_DATABASE_URI'] =  "sqlite:///" + os.path.join(basedir,"planets.db")
 
-db = sqlalchemy(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
+@app.cli.command("db_create")
+def db_create():
+    db.create_all()
+    print("Database created")
+
+
+@app.cli.command("db_drop")
+def db_drop():
+    db.drop_all()
+    print("Dropping the database")
+
+
+@app.cli.command("db_seed")
+def db_seed():
+
+    mercury = Planets(planet_name = "Mercury",
+                    planet_type = "Type D",
+                    home_star = "Sol",
+                    mass = 3.258e23,
+                    radius = 1516,
+                    distance = 3598e6
+                    )
+
+    venus = Planets(planet_name = "Venus",
+                    planet_type = "Type D",
+                    home_star = "Sol",
+                    mass = 3.258e24,
+                    radius = 1517,
+                    distance = 3598e6
+                    )
+
+    earth = Planets(planet_name = "Earth",
+                    planet_type = "Type M",
+                    home_star = "Sol",
+                    mass = 3.258e25,
+                    radius = 1518,
+                    distance = 3698e6
+                    )
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(
+                    first_name = "Ayush",
+                    last_name = "Yajnik",
+                    email = "test@test.com",
+                    password = "Password@123"
+                    )
+    
+    db.session.add(test_user)
+    db.session.commit()
+    print("Database seeded")
 
 # creating endpoint for index page
 @app.route("/")
@@ -46,9 +101,15 @@ def url_variables(name:str,age:int):
         if age < 18:
             return jsonify(message="Sorry " + name + " you are not welcomed"), 401
 
+@app.route("/planets")
+def planets():
+    planets_list = Planets.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(data = result)
+
 
 ## creating the ORM template for the database
-class User(db.model):
+class User(db.Model):
 
     __tablename__ = "users"
     id = Column(Integer)
@@ -57,7 +118,7 @@ class User(db.model):
     email = Column(String)
     password = Column(String)
 
-class planets(db.model):
+class Planets(db.Model):
 
     __tablename__ = "name"
     planet_id = Column(Integer, primary_key = True)
@@ -67,6 +128,25 @@ class planets(db.model):
     mass = Column(Float)
     distance = Column(Float)
     radius = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta():
+        fields = ("id", "first_name", "last_name", "email", "password")
+
+
+class PlanetSchema(ma.Schema):
+    class Meta():
+        fields = ("planet_id", "planet_name", "planet_type", "home_star", "mass","distance", "radius")
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
+    
 
 
 
